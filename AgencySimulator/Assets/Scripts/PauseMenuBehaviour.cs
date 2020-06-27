8c0aae48e0e177ef5b54740e1e8a39c3a04abdde
matwilli <matthew.williamson@cgifederal.com>
2020-06-27 17:02:04 -0500

    add pause menu

---
+++ AgencySimulator/Assets/Scripts/PauseMenuBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenuBehaviour : MonoBehaviour
+{
+    [SerializeField]
+ public   GameObject menuRoot;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuRoot.SetActive(!menuRoot.activeSelf);
+        }
+    }
+}
