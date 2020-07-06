4a2d0f5ca6c74596807b9d176fc71bd534857711
chuwilliamson <chuwilliamson@gmail.com>
2020-07-06 13:00:12 -0500

    remove interfaces for serialization to work properly

---
+++ AgencySimulator/Assets/ChuTools/Scripts/GameEvent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChuTools.Scripts
+{
+    [CreateAssetMenu]
+    public class GameEvent : ScriptableObject 
+    {
+        public List<GameEventListener> Listeners = new List<GameEventListener>();
+
+        public void Raise(params object[] args)
+        {
+            for (var i = Listeners.Count - 1; i >= 0; i--)
+                Listeners[i].OnEventRaised(args);
+        }
+
+        public void RegisterListener(GameEventListener listener)
+        {
+            if (Listeners.Contains(listener))
+            {
+                Debug.LogError("listener is already in list");
+                return;
+            }
+
+            Listeners.Add(listener);
+        }
+
+        public void UnregisterListener(GameEventListener listener)
+        {
+            if (!Listeners.Contains(listener))
+            {
+                Debug.LogError("listener is not in list");
+                return;
+            }
+
+            Listeners.Remove(listener);
+        }
+    }
+}
---
+++ AgencySimulator/Assets/ChuTools/Scripts/GameEventArgsListener.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChuTools.Scripts
+{
+    public class GameEventArgsListener : MonoBehaviour 
+    {
+        public GameEventListener GameEvent;
+        public List<GameEventArgsResponse> Responses;
+
+        public void OnEventRaised(object[] args)
+        {
+            Responses.ForEach(r => r.Invoke(args));
+        }
+
+        public void Subscribe()
+        {
+            GameEvent.Subscribe();
+        }
+
+        public void Unsubscribe()
+        {
+            GameEvent.Unsubscribe();
+        }
+
+        public virtual void OnEnable()
+        {
+            Subscribe();
+        }
+
+        public virtual void OnDisable()
+        {
+            Unsubscribe();
+        }
+    }
+}
