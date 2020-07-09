4caae0e0b32b4a08d355528677a89e343b8ddfd9
chuwilliamson <chuwilliamson@gmail.com>
2020-07-09 14:26:59 -0500

    make sliders adjusted the overall budget radial

---
+++ AgencySimulator/Assets/Scripts/SliderBudgetBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Michsky.UI.ModernUIPack;
+using UnityEngine;
+
+public class SliderBudgetBehaviour : MonoBehaviour
+{
+    public SliderManager slider;
+    
+    [SerializeField]
+    private float previousValue = 0;
+    
+    public RadialSlider radialSlider;
+    private void Start()
+    {
+        if (slider == null)
+            slider = GetComponent<SliderManager>();
+        
+        remaining = 100 - radialSlider.currentValue;
+    }
+
+    [SerializeField]
+    private float delta;
+    
+    [SerializeField]
+    private float remaining;
+    
+    public void OnValueChanged(float current)
+    {
+        delta = current - previousValue;
+        remaining = 100 - radialSlider.SliderValue;
+        if(delta > remaining)
+            slider.mainSlider.value = previousValue;
+        else
+        {
+            radialSlider.SliderValue += delta;
+            previousValue = current;
+        }
+        
+        
+        radialSlider.UpdateUI();
+        
+    }
+}
