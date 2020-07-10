dc3df174a7cfbe4c5fcf1cad4de7b5acf2eb76d6
chuwilliamson <chuwilliamson@gmail.com>
2020-07-10 11:07:45 -0500

    sliders now snap to the max possible value

--- AgencySimulator/Assets/Scripts/SliderBudgetBehaviour.cs
+++ AgencySimulator/Assets/Scripts/SliderBudgetBehaviour.cs
@@ -3,43 +3,56 @@ using System.Collections;
 using System.Collections.Generic;
 using Michsky.UI.ModernUIPack;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class SliderBudgetBehaviour : MonoBehaviour
 {
     public SliderManager slider;
-    
+
+    [FormerlySerializedAs("previousValue")]
     [SerializeField]
-    private float previousValue = 0;
-    
+    private float previous = 0;
+
     public RadialSlider radialSlider;
+
     private void Start()
     {
         if (slider == null)
             slider = GetComponent<SliderManager>();
-        
+        slider.sliderEvent.AddListener(OnValueChanged);
+
         remaining = 100 - radialSlider.currentValue;
     }
 
     [SerializeField]
     private float delta;
-    
+
     [SerializeField]
     private float remaining;
-    
+
     public void OnValueChanged(float current)
     {
-        delta = current - previousValue;
+        delta = current - previous;
         remaining = 100 - radialSlider.SliderValue;
-        if(delta > remaining)
-            slider.mainSlider.value = previousValue;
-        else
+        if (remaining <= 0 && delta > 0)
         {
-            radialSlider.SliderValue += delta;
-            previousValue = current;
+            slider.mainSlider.value = previous;
+            return;
         }
-        
-        
+
+        if (delta > remaining)//if the change in slider is more than what is left, try setting it to that amount
+        {
+            
+            
+            slider.mainSlider.value = previous + remaining;
+            current = slider.mainSlider.value;
+            delta = current - previous;
+        }
+
+        radialSlider.SliderValue += delta;
+        previous = current;
+
+
         radialSlider.UpdateUI();
-        
     }
 }
